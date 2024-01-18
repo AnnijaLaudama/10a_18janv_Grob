74aeb5d6d73621dcbd7a3ca5d206e3fea3457329
AnnijaLaudama <annija.laudama@grobinasskola.lv>
2024-01-18 13:12:05 +0200

    nr5

--- 4.uzdevums.py
+++ 4.uzdevums.py
@@ -1,1 +1,10 @@
-file=input("ievadifaila nosaukumu:")
+from tkinter.messagebox import showerror
+from tkinter.messagebox import showwarning
+from tkinter.messagebox import showinfo
+
+file=input("ievadifaila nosaukumu un veidni:")
+
+if not 
+    def showerror()
+    def showwarning()
+    def showinfo()
